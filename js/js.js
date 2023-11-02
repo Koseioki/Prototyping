e1b1212ff142776042745bddeec031f894756e0a
Kosei Oki <koseioki1225@gmail.com>
2023-11-02 09:31:26 +0100

    Map progress
    made some buttons active

--- js/js.js
+++ js/js.js
@@ -3,3 +3,18 @@
  * purpose: Behaviors
  **/
 console.log('Success: JavaScript from js/js.js running!')
+
+// when mousedown() is clicked, lightupRoom is lit
+function mousedown(){
+    document.getElementById("lightupStudyroom").style.display = "block";
+}
+function mouseup(){
+    document.getElementById("lightupStudyroom").style.display = "none";
+}
+
+function mousedown(){
+    document.getElementById("lightupBar").style.display = "block";
+}
+function mouseup(){
+    document.getElementById("lightupBar").style.display = "none";
+}
