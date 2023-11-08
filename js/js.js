f956b47a448a41510c7a13dc2ab55cd1d54d5c9e
Kosei Oki <koseioki1225@gmail.com>
2023-11-08 17:38:33 +0100

    a bit of changes

--- js/js.js
+++ js/js.js
@@ -7,7 +7,10 @@ console.log('Success: JavaScript from js/js.js running!')
 // when mousedown() is clicked, lightupRoom is lit
 function mousedown(){
     document.getElementById("lightupStudyroom").style.opacity = "1";
+    document.getElementById("overlay").style.opacity = "0.6";
+
 }
 function mouseup(){
     document.getElementById("lightupStudyroom").style.opacity = "0";
+    document.getElementById("overlay").style.opacity = "0";
 }
